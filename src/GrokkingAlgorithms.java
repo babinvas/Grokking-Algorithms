b2d3c37274258cac39d77dad628fc43e22e1a7bd
babinvas <babinvas@gmail.com>
2021-09-25 18:03:26 +0300

    Made some refactoring

--- src/GrokkingAlgorithms.java
+++ src/GrokkingAlgorithms.java
@@ -8,50 +8,46 @@ import java.util.NoSuchElementException;
 public class GrokkingAlgorithms {
 
 	public static void main(String[] args) {
-		binarySearch();
+		binarySearch(10, new int[]{-10, -2, 1, 3, 4, 10, 25});
 
-		smallestIndex();
+		smallestIndex(new Integer[]{5, 3, 6, 2, 10, -1, -100});
 
-		selectionSort();
+		selectionSort(new Integer[]{5, 3, 6, 2, 10, -1, -100});
 
-		countdownWitRecursion();
+		countdownWitRecursion(5);
 	}
 
-	private static void binarySearch() {
-		int[] array1 = new int[]{-10, -2, 1, 3, 4, 10, 25};
-		int item = 10;
-
+	private static void binarySearch(int item, int[] array) {
 		BinarySearch bs = new BinarySearch();
-		int itemIndex = bs.binarySearch(array1, item);
+		int itemIndex = bs.binarySearch(array, item);
 
 		try {
-			System.out.println(itemIndex +  " -> " + array1[itemIndex] + "\n");
+			System.out.println("Binary search for number " +  item +  " in the array. its index is " + itemIndex + ".\n");
 		} catch (NoSuchElementException e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
-	private static void smallestIndex() {
-		Integer[] array = new Integer[]{5, 3, 6, 2, 10, -1, -100};
-
+	private static void smallestIndex(Integer[] array) {
 		SelectionSort ss = new SelectionSort();
 		int smallestIndex = ss.findSmallest(array);
 
-		System.out.println(smallestIndex +  " -> " + array[smallestIndex] + "\n");
+		System.out.println("The smallest number of the array is " + array[smallestIndex] +  ". its index is " + smallestIndex + ".\n");
 	}
 
-	private static void selectionSort() {
-		Integer[] array = new Integer[]{5, 3, 6, 2, 10, -1, -100};
-
+	private static void selectionSort(Integer[] array) {
 		SelectionSort ss = new SelectionSort();
 
-		System.out.println(Arrays.toString(ss.selectionSort(array)) + "\n");
+		System.out.println("The selection sorting the array in ascending order: " + Arrays.toString(ss.selectionSort(array)) + ".\n");
 	}
 
-	private static void countdownWitRecursion() {
-		int from = 5;
+	private static void countdownWitRecursion(int from) {
+		System.out.println("Countdown using recursion: ");
 
 		Recursion recursion = new Recursion();
 		recursion.countdown(from);
+
+		System.out.println();
 	}
 }
+
